5b860468e34f243820b0161b0f6dd690732a37ba
ParancibiaASF <parancibia@aconcaguasf.com>
2021-04-07 12:43:52 -0300

    Added js files, parallax animation and navbar features

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+
+//******************* SCROLL DOWN ANIMATIONS *******************//
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+        
+        //------Functionality to change navbar color at certain distance from top----//
+        
+        // if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        //     document.getElementById("navbar").style.backgroundColor = "#7651ee";
+        // } else {
+        //     document.getElementById("navbar").style.backgroundColor = "transparent";
+        // }
+        
+    } else {
+        document.getElementById("navbar").style.top = "-80px";
+    }
+}
