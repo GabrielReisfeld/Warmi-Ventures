fda669ef56ea90351f4f7459cb782b0c398be852
Pablo Arancibia <parancibia.017@gmail.com>
2021-04-13 11:35:44 -0300

    Fixed position id on scroll down and parallax

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 
-const rellax = new Rellax('.rellax');
+// const rellax = new Rellax('.rellax');
 
 //******************* SCROLL DOWN ANIMATIONS *******************//
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollParallax()};
+// window.onscroll = function() {scrollFunction()};
+//
+// function scrollFunction() {
+//     //------Functionality to change navbar color at certain distance from top----//
+//
+//     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+//         document.getElementById("navbar").style.backgroundColor = "#7651ee";
+//     } else {
+//         document.getElementById("navbar").style.backgroundColor = "black";
+//     }    
+// }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
-        
-        //------Functionality to change navbar color at certain distance from top----//
-        
-        // if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        //     document.getElementById("navbar").style.backgroundColor = "#7651ee";
-        // } else {
-        //     document.getElementById("navbar").style.backgroundColor = "transparent";
-        // }
-        
-    } else {
-        document.getElementById("navbar").style.top = "-80px";
-    }
-}
+const el = document.querySelector("#parallax-bkg");
+
+window.addEventListener("scroll", (e) => {
+    // el.style.backgroundPositionX = -e.offsetX + "px";
+    el.style.backgroundPositionY = -window.pageYOffset + "px";
+});
