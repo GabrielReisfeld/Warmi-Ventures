d142b4701e180979116100770b834c7b732aba86
Pablo Arancibia <parancibia.017@gmail.com>
2021-04-20 10:35:57 -0300

    Fixed project after UI-UX feedback

--- js/index.js
+++ js/index.js
@@ -1,24 +1,8 @@
-
-// const rellax = new Rellax('.rellax');
-
-//******************* SCROLL DOWN ANIMATIONS *******************//
-
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// window.onscroll = function() {scrollFunction()};
-//
-// function scrollFunction() {
-//     //------Functionality to change navbar color at certain distance from top----//
-//
-//     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-//         document.getElementById("navbar").style.backgroundColor = "#7651ee";
-//     } else {
-//         document.getElementById("navbar").style.backgroundColor = "black";
-//     }    
-// }
-
 const el = document.querySelector("#parallax-bkg");
+var x = window.matchMedia("(max-width: 700px)")
 
 window.addEventListener("scroll", (e) => {
-    // el.style.backgroundPositionX = -e.offsetX + "px";
-    el.style.backgroundPositionY = -window.pageYOffset + "px";
+    if (!x.matches) {
+        el.style.backgroundPositionY = -window.pageYOffset + "px";
+    }
 });
